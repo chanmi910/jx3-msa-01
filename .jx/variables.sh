0e6d97f458b1b56239d65c72d9760dda76ebe444
chanmi910 <chanmi910@gmail.com>
2022-05-21 08:21:59 +0000

    update

--- .jx/variables.sh
+++ .jx/variables.sh
@@ -21,7 +21,5 @@ export REPO_OWNER='chanmi910'
 # content from git...
 export JX_PREVIEW_SERVICE="preview-svc"
 export JX_PREVIEW_PATH="/preview"
-#export MINK_IMAGE='docker.io/chanmi910/jx3-spring-demo:0.1'
-#export VERSION='0.1'
 
 
